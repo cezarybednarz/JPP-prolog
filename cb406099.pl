0e3478d2d2864f0150560d5369cc419d7123acaa
cezarybednarz <cezary.bednarz@gmail.com>
2021-06-03 20:05:23 +0200

    Reading input

--- cb406099.pl
+++ cb406099.pl
@@ -1,10 +1,31 @@
+:- ensure_loaded(library(lists)).
 
 verify :-
-    current_prolog_flag(argv, Arguments),
-    verify(Arguments).
+    current_prolog_flag(argv, [N,Program|_]),
+    verify(N, Program).
 
-verify(Argv) :-
-    concat_atom(Argv, ' ', SingleArg),
-    term_to_atom(Term, SingleArg),
-    Val is Term,
-    format('~w~n', [Val]).
+verify(N, Program) :-
+    seeing(OldStream),
+    see(Program),
+    repeat,
+    read(Term1),
+    read(Term2),
+    read(Term3),
+
+    write(N),
+    write("\n"),
+    write(Term1),
+    write("\n"),
+    write(Term2),
+    write("\n"),
+    write(Term3),
+    write("\n"),
+
+    seen,
+    see(OldStream).
+
+% initState(+Program, +N, -StanPoczątkowy)
+
+% step(+Program, +StanWe, ?PrId, -StanWy)
+
+
