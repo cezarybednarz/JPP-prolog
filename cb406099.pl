9c92b9f6f097926962bea2f5c2c83328426beb93
cezarybednarz <cezary.bednarz@gmail.com>
2021-06-04 22:56:53 +0200

    InitState

--- cb406099.pl
+++ cb406099.pl
@@ -1,16 +1,32 @@
 :- ensure_loaded(library(lists)).
 
 verify :-
+    debug,
     current_prolog_flag(argv, [N,Program|_]),
-    verify(N, Program).
+    atom_number(N, N_num),
+    verify(N_num, Program).
 
 verify(N, Program) :-
     see(Program),
     read(Term1),
-    Term1 =.. [_, Variables],
     read(Term2),
-    Term2 =.. [_, Arrays],
     read(Term3),
+    Term1 =.. [_, Variables],
+    Term2 =.. [_, Arrays],
     Term3 =.. [_, Prog],
+    initState(N, Prog, InitState),
+    write(InitState),
     seen.
+
+storage(vars, arrs, line).
+
+state(n, prog, storages, visitedStorages).
+
+build(X, N, List) :- 
+    length(List, N), 
+    maplist(=(X), List).
+
+initState(N, Prog, State) :-
+    build(storage([], [], 0), N, InitStorages),
+    State = state(N, Prog, InitStorages, []).
 
